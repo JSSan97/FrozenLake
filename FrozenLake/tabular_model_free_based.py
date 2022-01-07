1ce45551b557798f09a666410a11123c5e4bac4d
JSSan97 <j.san@hotmail.co.uk>
2022-01-07 17:26:06 +0000

    Question 5

--- FrozenLake/tabular_model_free_based.py
+++ FrozenLake/tabular_model_free_based.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tabular_model_based import policy_evaluation
 
 def epsilon_greedy_policy(env, state, epsilon, q):
     if np.random.uniform(0, 1) < 1 - epsilon:
@@ -23,6 +24,17 @@ def sarsa(env, max_episodes, eta, gamma, epsilon, seed=None):
 
     q = np.zeros((env.n_states, env.n_actions))
 
+    values = []
+
+    expected = [[0.189, 0.212, 0.237, 0.266, 0.298, 0.334, 0.374, 0.418],
+                [0.203, 0.227, 0.256, 0.288, 0.332, 0.373, 0.419, 0.469],
+                [0.203, 0.227, 0.25,  0.,    0.356, 0.408, 0.47,  0.529],
+                [0.226, 0.255, 0.288, 0.325, 0.383, 0.,    0.519, 0.595],
+                [0.202, 0.222, 0.244, 0.,    0.441, 0.507, 0.581, 0.671],
+                [0.176, 0.,    0.,    0.417, 0.491, 0.567, 0.,    0.756],
+                [0.176, 0.,    0.301, 0.354, 0.,    0.654, 0.,    0.869],
+                [0.202, 0.227, 0.261, 0.,    0.656, 0.771, 0.869, 1.   ]]
+
     for i in range(max_episodes):
         s = env.reset()
         action = epsilon_greedy_policy(env, s, epsilon[i], q)
@@ -34,6 +46,10 @@ def sarsa(env, max_episodes, eta, gamma, epsilon, seed=None):
             q[s][action] = q[s][action] + (eta[i] * (reward + (gamma * q[next_state][next_action]) - q[s][action]))
             s = next_state
             action = next_action
+
+        if np.equal(q.max(axis=1), expected):
+            print("sarsa episode optimal {}".format(i))
+
 
     policy = q.argmax(axis=1)
     value = q.max(axis=1)
@@ -48,6 +64,17 @@ def q_learning(env, max_episodes, eta, gamma, epsilon, seed=None):
 
     q = np.zeros((env.n_states, env.n_actions))
 
+    values = []
+
+    expected = [[0.189, 0.212, 0.237, 0.266, 0.298, 0.334, 0.374, 0.418],
+                [0.203, 0.227, 0.256, 0.288, 0.332, 0.373, 0.419, 0.469],
+                [0.203, 0.227, 0.25,  0.,    0.356, 0.408, 0.47,  0.529],
+                [0.226, 0.255, 0.288, 0.325, 0.383, 0.,    0.519, 0.595],
+                [0.202, 0.222, 0.244, 0.,    0.441, 0.507, 0.581, 0.671],
+                [0.176, 0.,    0.,    0.417, 0.491, 0.567, 0.,    0.756],
+                [0.176, 0.,    0.301, 0.354, 0.,    0.654, 0.,    0.869],
+                [0.202, 0.227, 0.261, 0.,    0.656, 0.771, 0.869, 1.   ]]
+
     for i in range(max_episodes):
         s = env.reset()
         done = False
@@ -56,6 +83,9 @@ def q_learning(env, max_episodes, eta, gamma, epsilon, seed=None):
             next_state, reward, done = env.step(action)
             q[s][action] = q[s][action] + (eta[i] * (reward + (gamma*np.max(q[next_state])) - q[s][action]))
             s = next_state
+
+        if np.equal(q.max(axis=1), expected):
+            print("sarsa episode optimal {}".format(i))
 
     policy = q.argmax(axis=1)
     value = q.max(axis=1)
